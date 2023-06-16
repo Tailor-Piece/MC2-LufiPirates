010d73fa2c2c9ce897658ca7b4f743b2d2357ab6
saepasomba <saepasomba12@gmail.com>
2023-06-16 14:15:15 +0700

    Initial Commit

---
+++ Tailor Piece/Tailor_PieceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tailor_PieceApp.swift
+//  Tailor Piece
+//
+//  Created by Sae Pasomba on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Tailor_PieceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
